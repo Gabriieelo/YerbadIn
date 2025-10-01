49f5b95a5836c80ddbb1cd8907acba4079280e92
Gabriieelo <gabrieldelgado183@icloud.com>
2025-09-30 21:28:51 -0300

    Ac

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.getElementById("buscador");
+  const cards = document.querySelectorAll(".card");
+
+  input.addEventListener("keyup", () => {
+    const filtro = input.value.toLowerCase();
+
+    cards.forEach(card => {
+      const titulo = card.querySelector(".card-title").textContent.toLowerCase();
+      const texto = card.querySelector(".card-text").textContent.toLowerCase();
+
+      if (titulo.includes(filtro) || texto.includes(filtro)) {
+        card.parentElement.style.display = "block"; // mostrar columna
+      } else {
+        card.parentElement.style.display = "none"; // ocultar columna
+      }
+    });
+  });
+});
